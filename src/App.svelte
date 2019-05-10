23fd1512843e2e503bf8ad17abd00c6343280079
Brandon Cox <brandon.cox@MBPR15-S04446.local>
2019-05-10 14:51:26 -0400

    inter

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,7 @@
 
 <svelte:head>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css" />
+  <link rel="stylesheet" href="./inter.css" />
 </svelte:head>
 
 <main class="content">
